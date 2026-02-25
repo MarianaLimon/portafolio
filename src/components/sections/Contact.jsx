a565dcb9e9d4510347e605c57bb22169972682fb
MarianaLimon <marianalimong@hotmail.com>
2026-02-25 14:00:22 -0600

    construccion de contacto

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,11 +1,58 @@
 import SectionCard from "../ui/SectionCard";
 import SectionTitle from "../ui/SectionTitle";
+import "../../css/contact-section.css";
 
-export default function Contant() {
+import { FiMail, FiPhone, FiMapPin } from "react-icons/fi";
+import { FaWhatsapp, FaLinkedin } from "react-icons/fa";
+
+export default function Contact() {
   return (
     <SectionCard>
+
       <SectionTitle>Contacto</SectionTitle>
-      <p>Contenido de contacto...</p>
+
+      <div className="contact-list">
+
+        <a href="mailto:marianalimong@hotmail.com">
+          <FiMail className="contact-icon" />
+          marianalimong@hotmail.com
+        </a>
+
+        <a href="mailto:marianalimong92@gmail.com">
+          <FiMail className="contact-icon" />
+          marianalimong92@gmail.com
+        </a>
+
+        <a href="tel:+525612647883">
+          <FiPhone className="contact-icon" />
+          56 1264 7883
+        </a>
+
+        <a
+          href="https://wa.me/525612647883"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaWhatsapp className="contact-icon" />
+          WhatsApp
+        </a>
+
+        <span>
+          <FiMapPin className="contact-icon" />
+          Coacalco, Edo. México
+        </span>
+
+        <a
+          href="https://www.linkedin.com/in/mariana-limon-932053134"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="contact-icon" />
+          LinkedIn
+        </a>
+
+      </div>
+
     </SectionCard>
   );
 }
